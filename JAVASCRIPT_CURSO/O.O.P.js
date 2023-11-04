ee0945a4d69d66b1e204cda51d3d37b76f2f4a91
Nosligedoc <gilsonbrito80@gmail.com>
2023-11-03 22:49:53 -0300

    O.O.P

--- JAVASCRIPT_CURSO/O.O.P.js
+++ JAVASCRIPT_CURSO/O.O.P.js
@@ -1,4 +1,5 @@
 
+/*
 // ORIENTAÇÃO Á OBJETOS
 
 // Quando for apenas um objeto usar apenas { } , quando for um arrays de objetos usar [{}]
@@ -61,3 +62,89 @@ for (let i = 0; i < carros.length; i++) {  // FOR pra percorrer uma Lista de for
     // console.log(i); // Imprimir o Indice 
     console.log(element, i); // Imprimir o dado do indice
 };
+
+*/
+
+// ISNTANCIANDO OBJETOS UTILIZANDO FUNÇÃO JAVASCRIPT
+/*
+function carro1 (fabricante,modelo,cor,anoFabricacao){
+    fabricante = fabricante;
+    modelo = modelo;
+    cor = cor;
+    anoFabricacao = anoFabricacao;
+    return {fabricante,modelo,cor,anoFabricacao}
+}
+var  carroNovo = new carro1 ('Fiat','Uno','Vermelho',2015,);
+console.log(carroNovo);
+*/
+
+// INSTANCIANDO OBJETOS UTILIZANDO CLASSES JAVASCRIPT
+
+class Carro {
+    constructor (fabricante,modelo,cor,anoFabricacao){
+    this.fabricante = fabricante;
+    this.modelo = modelo;
+    this.cor = cor;
+    this.anoFabricacao = anoFabricacao;
+    }
+
+    get _fabricante(){return this.fabricante}
+    get _modelo(){return this.modelo}
+    get _cor(){return this.cor}
+    get _anoFabricacao(){return this.anoFabricacao}
+
+    set _SetModelo(NovoModelo){
+        this.modelo = NovoModelo;
+    }
+    set _SetCor(NovaCor){
+        this.cor = NovaCor;
+    }
+
+    set _SetFabricante(NovaFabricante){
+        this.fabricante = NovaFabricante;
+    }
+
+    set _SetAnoFabricação(NovoAnoFabricacao){
+        this.anoFabricacao = NovoAnoFabricacao;
+    }
+    }
+
+
+
+const BMW = new Carro('BMW','X1','BRANCO',2023);
+BMW._SetModelo = '320I';
+BMW._SetCor = 'Azul';
+BMW._SetAnoFabricação = 2024;
+BMW._SetFabricante = 'Audi'
+
+
+
+console.log(BMW._modelo,BMW._cor, BMW._fabricante,BMW._anoFabricacao);
+
+
+
+class Pessoa{
+    constructor(nome,sobrenome,idade,dataDeNascimento){
+        this.nome = nome; this.sobrenome = sobrenome; this.idade = idade; this.dataDeNascimento= dataDeNascimento;
+    }
+}
+
+class Animal{
+    constructor(nome,raca,cor,idade){
+        this.nome = nome; this.raca = raca; this.cor = cor; this.idade = idade;
+    }
+}
+
+class NomeObjeto{
+    constructor(atributo1,atributo2,atributo3){
+        this.atributo1 = atributo1; this.atributo2 = atributo2; this.atributo3 = atributo3;
+    }
+}
+
+
+var pessoa01 = new Pessoa('Vanderlei','Silva Rocha',25, '07 06 1993');
+
+
+console.log(pessoa01);
+
+
